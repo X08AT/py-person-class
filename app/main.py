819ca8dbe32742650164444f2283f718a4ddd870
Artem Turchyn <Artem.at131@gmail.com>
2026-02-06 16:11:03 +0200

    solved

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    person_list = []
+
+    for entity in people:
+        human = Person(entity["name"], entity["age"])
+        person_list.append(human)
+
+    for entity in people:
+        human = Person.people[entity["name"]]
+        if "wife" in entity and entity["wife"] is not None:
+            human.wife = Person.people[entity["wife"]]
+        if "husband" in entity and entity["husband"] is not None:
+            human.husband = Person.people[entity["husband"]]
+
+    return person_list
